7b13f2e54eef327d2319c17fe8226047e859157e
David Ward <davidmward@mail.com>
2024-06-18 17:52:32 -0700

    add files from smalltest-draft

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Import user libraries here
+#include "foo.h"
+
+int main(int argc, char** argv)
+{
+        // This would be body of program
+        message("Begin program.");
+
+        return 0;
+}
---
+++ test.h
@@ -0,0 +1,11 @@
+#ifndef TEST_H
+#define TEST_H
+
+// Testing user functions
+void test_abs_pass(int input_value, int expected_value);
+
+void test_abs_fail(int input_value, int expected_value);
+
+void run_tests();
+
+#endif
